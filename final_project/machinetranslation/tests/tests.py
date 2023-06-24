f8793575cb6b0f695aae796f702cbe9326e0d549
huzaifa-qureshi <huzaifa981981@gmail.com>
2023-06-24 17:50:31 +0500

    added docker file

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -4,9 +4,10 @@ from  translator import english_to_french , french_to_english
 class TestE2F(unittest.TestCase):
     def test_englishToFrench(self):
         self.assertEqual(english_to_french("Hello."), "Bonjour.")
-
+        self.assertNotEqual(english_to_french("Hello."), "Something")
 class TestF2E(unittest.TestCase):
     def test_frenchToEnglish(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(english_to_french("Bonjour"), "Something")
 
 unittest.main()
